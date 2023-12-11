c10827e8e7b487a3dbf26a5dabb2475a636e9f81
narengithub009 <narender.kon09@gmail.com>
2023-12-11 21:45:44 +0530

    Hello

---
+++ question-service/src/main/java/com/java/question/QuestionServiceApplication.java
@@ -0,0 +1,16 @@
+package com.java.question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionServiceApplication.class, args);
+	}
+	static void MyTest() {
+		System.out.println("Hello.........");
+	}
+
+}
